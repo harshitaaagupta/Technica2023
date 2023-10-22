6d42933b6cd128e9416be629730b85a6ac897577
Spriha Jha <jha.spriha@gmail.com>
2023-10-22 08:48:52 -0400

    Add script to create footnotes

--- scripts/app.py
+++ scripts/app.py
@@ -9,33 +9,40 @@ from langchain.chains import RetrievalQA
 import os
 from flask_cors import CORS
 
-os.environ["OPENAI_API_KEY"] = ""
+# Set OpenAI API key as an environment variable
+os.environ["OPENAI_API_KEY"] = "your-openai-api-key"
 
-# Document loader
-loader = PyPDFDirectoryLoader(path='../10-K')
+# Load documents from specified directory
+loader = PyPDFDirectoryLoader(path='path/to/your/pdf/directory')
 data = loader.load()
 
-# Split
+# Initialize text splitter with specified chunk size and overlap
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=4000, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
 
-# Store
+# Create a vector store from the split documents using OpenAI embeddings
 vectorstore = Chroma.from_documents(
     documents=all_splits, embedding=OpenAIEmbeddings())
 
-# Initialize the ChatOpenAI model
+# Initialize ChatOpenAI model with specified parameters
 llm = ChatOpenAI(model_name="gpt-4", temperature=0.7,
                  max_tokens=500, verbose=True)
 
 app = Flask(__name__)
 CORS(app)
 
+# Define a route for creating footnotes
+
+
 @app.route('/create-footnotes', methods=['POST'])
-def create_draft():
+def create_footnotes():
+    # Get the query from the request
     query = request.json.get('query')
+    # Get the template from the request
     template = request.json.get('template')
 
+    # Return error if template is missing
     if not template:
         return jsonify({"error": "Template parameter is missing!"}), 400
 
@@ -43,12 +50,13 @@ def create_draft():
     QA_CHAIN_PROMPT = PromptTemplate(
         input_variables=["context", "question"], template=template)
 
-    # Run chain
+    # Run the RetrievalQA chain with the provided prompt
     qa_chain = RetrievalQA.from_chain_type(llm,
                                            retriever=vectorstore.as_retriever(),
                                            chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
 
     result = qa_chain({"query": query})
+    # Return the result as a JSON response
     return jsonify(result["result"])
 
 
